97f7e92125558f3e4cf69b049a27dfc230e7877d
TangXinGithub <48245380+TangXinGithub@users.noreply.github.com>
2020-08-11 09:35:27 +0800

    init

--- app/src/main/java/com/zhu/cactus/App.kt
+++ app/src/main/java/com/zhu/cactus/App.kt
@@ -5,11 +5,11 @@ import com.gyf.cactus.callback.CactusCallback
 
 class App: Application(),CactusCallback {
     override fun doWork(times: Int) {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 
     override fun onStop() {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
     }
 //    https://juejin.im/entry/6844903496806825998  Application的介绍 单例模式 Android系统的入口是Application类的 onCreate（），默认为空实现
 }
