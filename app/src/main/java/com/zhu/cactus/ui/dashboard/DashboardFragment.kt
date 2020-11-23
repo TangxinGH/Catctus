cf1fda7351579b4ba47439498ac73158be08ee3f
TangXinGithub <48245380+TangXinGithub@users.noreply.github.com>
2020-11-23 23:12:53 +0800

    暂时progress先

--- app/src/main/java/com/zhu/cactus/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/zhu/cactus/ui/dashboard/DashboardFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -50,58 +51,57 @@ class DashboardFragment : Fragment() {
         }
 
 
+        /*  root.join_to_act.setOnClickListener {
+              if (edit_join_id2.text != null && edit_join_id2.text.isNotBlank()) {
+                  val daomeng = App(com.zhu.cactus.App.context, dashboardViewModel.text).join(
+                      edit_join_id2.text.toString(),
+                      numberPicker.value
+                  )//报名活动
+              } else Toast.makeText(
+                  context,
+                  "id输入为空了或者 “”:`${edit_join_id2}`",
+                  Toast.LENGTH_SHORT
+              ).show()
+          }
+          root.only_me_join.setOnClickListener {
+              val daomeng = App(com.zhu.cactus.App.context, dashboardViewModel.text)
+              if (daomeng.is_login()) {
+                  daomeng.can_join()
+              }
+          }
+          root.act_info.setOnClickListener {
+              val actInfo = App(com.zhu.cactus.App.context, dashboardViewModel.text)
+              if (actInfo.is_login() && edit_join_id2.text.isNotBlank()) {
+                  actInfo.chiken(edit_join_id2.text.toString())
+                  Toast.makeText(com.zhu.cactus.App.context, "wait ", Toast.LENGTH_SHORT).show()
+              } else Toast.makeText(com.zhu.cactus.App.context, "未登录 或者 id 为空 ", Toast.LENGTH_SHORT)
+                  .show()
+          }
 
-      /*  root.join_to_act.setOnClickListener {
-            if (edit_join_id2.text != null && edit_join_id2.text.isNotBlank()) {
-                val daomeng = App(com.zhu.cactus.App.context, dashboardViewModel.text).join(
-                    edit_join_id2.text.toString(),
-                    numberPicker.value
-                )//报名活动
-            } else Toast.makeText(
-                context,
-                "id输入为空了或者 “”:`${edit_join_id2}`",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-        root.only_me_join.setOnClickListener {
-            val daomeng = App(com.zhu.cactus.App.context, dashboardViewModel.text)
-            if (daomeng.is_login()) {
-                daomeng.can_join()
-            }
-        }
-        root.act_info.setOnClickListener {
-            val actInfo = App(com.zhu.cactus.App.context, dashboardViewModel.text)
-            if (actInfo.is_login() && edit_join_id2.text.isNotBlank()) {
-                actInfo.chiken(edit_join_id2.text.toString())
-                Toast.makeText(com.zhu.cactus.App.context, "wait ", Toast.LENGTH_SHORT).show()
-            } else Toast.makeText(com.zhu.cactus.App.context, "未登录 或者 id 为空 ", Toast.LENGTH_SHORT)
-                .show()
-        }
-
-        root.cancel_action.setOnClickListener {
-            val act_cancel = App(com.zhu.cactus.App.context, dashboardViewModel.text)
-            if (act_cancel.is_login() && edit_join_id2.text.isNotBlank()) {
-                act_cancel.concle(edit_join_id2.text.toString())
-            }
-        }
-*/
+          root.cancel_action.setOnClickListener {
+              val act_cancel = App(com.zhu.cactus.App.context, dashboardViewModel.text)
+              if (act_cancel.is_login() && edit_join_id2.text.isNotBlank()) {
+                  act_cancel.concle(edit_join_id2.text.toString())
+              }
+          }
+  */
         val menu = root.circle_menu
         menu.eventListener = object : CircleMenuView.EventListener() {
-        /*    override fun onMenuOpenAnimationStart(view: CircleMenuView) {
-                Log.d("D", "onMenuOpenAnimationStart")
-            }
+            /*    override fun onMenuOpenAnimationStart(view: CircleMenuView) {
+                    Log.d("D", "onMenuOpenAnimationStart")
+                }
 
-            override fun onMenuOpenAnimationEnd(view: CircleMenuView) {
-                Log.d("D", "onMenuOpenAnimationEnd")
-            }
+                override fun onMenuOpenAnimationEnd(view: CircleMenuView) {
+                    Log.d("D", "onMenuOpenAnimationEnd")
+                }
 
-            override fun onMenuCloseAnimationStart(view: CircleMenuView) {
-                Log.d("D", "onMenuCloseAnimationStart")
-            }
+                override fun onMenuCloseAnimationStart(view: CircleMenuView) {
+                    Log.d("D", "onMenuCloseAnimationStart")
+                }
 
-            override fun onMenuCloseAnimationEnd(view: CircleMenuView) {
-                Log.d("D", "onMenuCloseAnimationEnd")
-            }*/
+                override fun onMenuCloseAnimationEnd(view: CircleMenuView) {
+                    Log.d("D", "onMenuCloseAnimationEnd")
+                }*/
 
             override fun onButtonClickAnimationStart(view: CircleMenuView, index: Int) {
                 Log.d("D", "onButtonClickAnimationStart| index: $index")
@@ -121,7 +121,7 @@ class DashboardFragment : Fragment() {
                         if (edit_join_id2.text != null && edit_join_id2.text.isNotBlank()) {
                             App(com.zhu.cactus.App.context, dashboardViewModel.text).join(
                                 edit_join_id2.text.toString(),
-                                numberPicker.value
+                                sleep_SeekBar.progress
                             )//报名活动
                         } else Toast.makeText(
                             context,
@@ -133,8 +133,13 @@ class DashboardFragment : Fragment() {
                         val actInfo = App(com.zhu.cactus.App.context, dashboardViewModel.text)
                         if (actInfo.is_login() && edit_join_id2.text.isNotBlank()) {
                             actInfo.chiken(edit_join_id2.text.toString())
-                            Toast.makeText(com.zhu.cactus.App.context, "wait ", Toast.LENGTH_SHORT).show()
-                        } else Toast.makeText(com.zhu.cactus.App.context, "未登录 或者 id 为空 ", Toast.LENGTH_SHORT)
+                            Toast.makeText(com.zhu.cactus.App.context, "wait ", Toast.LENGTH_SHORT)
+                                .show()
+                        } else Toast.makeText(
+                            com.zhu.cactus.App.context,
+                            "未登录 或者 id 为空 ",
+                            Toast.LENGTH_SHORT
+                        )
                             .show()
                     }
                     "仅我" -> {
@@ -149,13 +154,30 @@ class DashboardFragment : Fragment() {
                             act_cancel.concle(edit_join_id2.text.toString())
                         }
                     }
-                    else -> Log.d("fabmenu"," 什么都不是失败")
+                    else -> Log.d("fabmenu", " 什么都不是失败")
                 }
             }
         })
-        root.numberPicker.maxValue = 900
-        root.numberPicker.minValue = 70
-        root.numberPicker.value = 200 //人的反应为0.2秒
+
+
+       root. sleep_SeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+           override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textMin5.text= progress.toString()//设置值
+           }
+
+           override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+           }
+
+           override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+           }
+
+       })
+
+//        root.numberPicker.maxValue = 900
+//        root.numberPicker.minValue = 70
+//        root.numberPicker.value = 200 //人的反应为0.2秒
 
         if (com.zhu.cactus.App.typeface != null) TypefaceHelper.typeface(root)//应用字体
 
