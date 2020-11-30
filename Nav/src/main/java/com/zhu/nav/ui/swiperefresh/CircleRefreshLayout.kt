8dae9a8eb2addc7794f05e445708cfa505876bd6
TangXinGithub <48245380+TangXinGithub@users.noreply.github.com>
2020-11-30 15:21:02 +0800

    下拉刷新 沉浸栏 login null to kotlin

--- Nav/src/main/java/com/zhu/nav/ui/swiperefresh/CircleRefreshLayout.kt
+++ Nav/src/main/java/com/zhu/nav/ui/swiperefresh/CircleRefreshLayout.kt
@@ -1,276 +1,232 @@
-package com.zhu.nav.ui.swiperefresh;
+package com.zhu.nav.ui.swiperefresh
 
-import android.animation.ValueAnimator;
-import android.content.Context;
-import android.content.res.TypedArray;
-import android.util.AttributeSet;
-import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.FrameLayout;
-
-import androidx.annotation.NonNull;
-import androidx.core.view.ViewCompat;
-
-import com.zhu.nav.R;
+import android.animation.ValueAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import com.zhu.nav.R
 
 /**
  * Created by zhanglei on 15/7/20.
  */
-public class CircleRefreshLayout extends FrameLayout {
-
-    private static String TAG = "pullToRefresh";
-
-    private static final long BACK_TOP_DUR = 600;
-    private static final long REL_DRAG_DUR = 200;
-
-    private int mHeaderBackColor = 0xff8b90af;
-    private int mHeaderForeColor = 0xffffffff;
-    private int mHeaderCircleSmaller = 6;
-
-
-    private float mPullHeight;
-    private float mHeaderHeight;
-    private View mChildView;
-    protected AnimationView mHeader;
-
-    public AnimationView getmHeader() {
-        return mHeader;
+class CircleRefreshLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    private var mHeaderBackColor = -0x746f51
+    private var mHeaderForeColor = -0x1
+    private var mHeaderCircleSmaller = 6
+    private var mPullHeight = 0f
+    private var mHeaderHeight = 0f
+    private var mChildView: View? = null
+    protected var mHeader: AnimationView? = null
+    fun getmHeader(): AnimationView? {
+        return mHeader
     }
 
-    private boolean mIsRefreshing;
-
-    private float mTouchStartY;
-
-    private float mTouchCurY;
-
-    private ValueAnimator mUpBackAnimator;
-    private ValueAnimator mUpTopAnimator;
-
-    private DecelerateInterpolator decelerateInterpolator = new DecelerateInterpolator(10);
-
-    public CircleRefreshLayout(Context context) {
-        this(context, null, 0);
-    }
-
-    public CircleRefreshLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-
-    public CircleRefreshLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr);
-    }
-
-    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
-
-        if (getChildCount() > 1) {
-            throw new RuntimeException("you can only attach one child");
+    private var mIsRefreshing = false
+    private var mTouchStartY = 0f
+    private var mTouchCurY = 0f
+    private var mUpBackAnimator: ValueAnimator? = null
+    private var mUpTopAnimator: ValueAnimator? = null
+    private val decelerateInterpolator = DecelerateInterpolator(10F)
+    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+        if (childCount > 1) {
+            throw RuntimeException("you can only attach one child")
         }
-        setAttrs(attrs);
-        mPullHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, context.getResources().getDisplayMetrics());
-        mHeaderHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, context.getResources().getDisplayMetrics());
-
-        this.post(new Runnable() {
-            @Override
-            public void run() {
-                mChildView = getChildAt(0);
-                addHeaderView();
-            }
-        });
-
+        setAttrs(attrs)
+        mPullHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            150f,
+            context.resources.displayMetrics
+        )
+        mHeaderHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            100f,
+            context.resources.displayMetrics
+        )
+        post {
+            mChildView = getChildAt(0)
+            addHeaderView()
+        }
     }
 
-    private void setAttrs(AttributeSet attrs) {
-        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CirCleRefreshLayout);
-
-        mHeaderBackColor = a.getColor(R.styleable.CirCleRefreshLayout_AniBackColor, mHeaderBackColor);
-        mHeaderForeColor = a.getColor(R.styleable.CirCleRefreshLayout_AniForeColor, mHeaderForeColor);
-        mHeaderCircleSmaller = a.getInt(R.styleable.CirCleRefreshLayout_CircleSmaller, mHeaderCircleSmaller);
-
-        a.recycle();
+    private fun setAttrs(attrs: AttributeSet?) {
+        val a = context.obtainStyledAttributes(attrs, R.styleable.CirCleRefreshLayout)
+        mHeaderBackColor =
+            a.getColor(R.styleable.CirCleRefreshLayout_AniBackColor, mHeaderBackColor)
+        mHeaderForeColor =
+            a.getColor(R.styleable.CirCleRefreshLayout_AniForeColor, mHeaderForeColor)
+        mHeaderCircleSmaller =
+            a.getInt(R.styleable.CirCleRefreshLayout_CircleSmaller, mHeaderCircleSmaller)
+        a.recycle()
     }
 
-    private void addHeaderView() {
-        mHeader = new AnimationView(getContext());
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0);
-        params.gravity = Gravity.TOP;
-        mHeader.setLayoutParams(params);
-
-        addViewInternal(mHeader);
-        mHeader.setAniBackColor(mHeaderBackColor);
-        mHeader.setAniForeColor(mHeaderForeColor);
-        mHeader.setRadius(mHeaderCircleSmaller);
-
-        setUpChildAnimation();
+    private fun addHeaderView() {
+        mHeader = AnimationView(context)
+        val params = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0)
+        params.gravity = Gravity.TOP
+        mHeader!!.layoutParams = params
+        addViewInternal(mHeader!!)
+        mHeader!!.setAniBackColor(mHeaderBackColor)
+        mHeader!!.setAniForeColor(mHeaderForeColor)
+        mHeader!!.setRadius(mHeaderCircleSmaller)
+        setUpChildAnimation()
     }
 
-    private void setUpChildAnimation() {
+    private fun setUpChildAnimation() {
         if (mChildView == null) {
-            return;
+            return
         }
-        mUpBackAnimator = ValueAnimator.ofFloat(mPullHeight, mHeaderHeight);
-        mUpBackAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float val = (float) animation.getAnimatedValue();
-                if (mChildView != null) {
-                    mChildView.setTranslationY(val);
-                }
+        val mUpBackAnimator = ValueAnimator.ofFloat(mPullHeight, mHeaderHeight)
+        mUpBackAnimator.addUpdateListener { animation ->
+            val `val` = animation.animatedValue as Float
+            if (mChildView != null) {
+                mChildView!!.translationY = `val`
             }
-        });
-        mUpBackAnimator.setDuration(REL_DRAG_DUR);
-        mUpTopAnimator = ValueAnimator.ofFloat(mHeaderHeight, 0);
-        mUpTopAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                float val = (float) animation.getAnimatedValue();
-                val = decelerateInterpolator.getInterpolation(val / mHeaderHeight) * val;
-                if (mChildView != null) {
-                    mChildView.setTranslationY(val);
-                }
-                mHeader.getLayoutParams().height = (int) val;
-                mHeader.requestLayout();
-            }
-        });
-        mUpTopAnimator.setDuration(BACK_TOP_DUR);
-
-        mHeader.setOnViewAniDone(new AnimationView.OnViewAniDone() {
-            @Override
-            public void viewAniDone() {
-//                Log.i(TAG, "should invoke");
-                mUpTopAnimator.start();
-            }
-        });
-
-
-    }
-
-    private void addViewInternal(@NonNull View child) {
-        super.addView(child);
-    }
-
-    @Override
-    public void addView(View child) {
-        if (getChildCount() >= 1) {
-            throw new RuntimeException("you can only attach one child");
         }
+        mUpBackAnimator.duration = REL_DRAG_DUR
+        this.mUpBackAnimator =mUpBackAnimator
 
-        mChildView = child;
-        super.addView(child);
-        setUpChildAnimation();
-    }
-
-    private boolean canChildScrollUp() {
-        if (mChildView == null) {
-            return false;
+        val mUpTopAnimator = ValueAnimator.ofFloat(mHeaderHeight, 0f)
+        mUpTopAnimator.addUpdateListener { animation ->
+            var `val` = animation.animatedValue as Float
+            `val` *= decelerateInterpolator.getInterpolation(`val` / mHeaderHeight)
+            if (mChildView != null) {
+                mChildView!!.translationY = `val`
+            }
+            mHeader!!.layoutParams.height = `val`.toInt()
+            mHeader!!.requestLayout()
         }
-
-
-        return ViewCompat.canScrollVertically(mChildView, -1);
+        mUpTopAnimator.duration = BACK_TOP_DUR
+        mHeader!!.setOnViewAniDone { //                Log.i(TAG, "should invoke");
+            mUpTopAnimator.start()
+        }
+        this.mUpTopAnimator =mUpTopAnimator
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
+    private fun addViewInternal(child: View) {
+        super.addView(child)
+    }
+
+    override fun addView(child: View) {
+        if (childCount >= 1) {
+            throw RuntimeException("you can only attach one child")
+        }
+        mChildView = child
+        super.addView(child)
+        setUpChildAnimation()
+    }
+
+    private fun canChildScrollUp(): Boolean {
+        return if (mChildView == null) {
+            false
+        } else ViewCompat.canScrollVertically(mChildView, -1)
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         if (mIsRefreshing) {
-            return true;
+            return true
         }
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                mTouchStartY = ev.getY();
-                mTouchCurY = mTouchStartY;
-                break;
-            case MotionEvent.ACTION_MOVE:
-                float curY = ev.getY();
-                float dy = curY - mTouchStartY;
+        when (ev.action) {
+            MotionEvent.ACTION_DOWN -> {
+                mTouchStartY = ev.y
+                mTouchCurY = mTouchStartY
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val curY = ev.y
+                val dy = curY - mTouchStartY
                 if (dy > 0 && !canChildScrollUp()) {
-                    return true;
+                    return true
                 }
+            }
         }
-        return super.onInterceptTouchEvent(ev);
+        return super.onInterceptTouchEvent(ev)
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (mIsRefreshing) {
-            return super.onTouchEvent(event);
-        }
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_MOVE:
-                mTouchCurY = event.getY();
-                float dy = mTouchCurY - mTouchStartY;
-                dy = Math.min(mPullHeight * 2, dy);
-                dy = Math.max(0, dy);
-
-
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (mIsRefreshing) {
+            super.onTouchEvent(event)
+        } else when (event.action) {
+            MotionEvent.ACTION_MOVE -> {
+                mTouchCurY = event.y
+                var dy = mTouchCurY - mTouchStartY
+                dy = Math.min(mPullHeight * 2, dy)
+                dy = Math.max(0f, dy)
                 if (mChildView != null) {
-                    float offsetY = decelerateInterpolator.getInterpolation(dy / 2 / mPullHeight) * dy / 2;
-                    mChildView.setTranslationY(offsetY);
-
-                    mHeader.getLayoutParams().height = (int) offsetY;
-                    mHeader.requestLayout();
+                    val offsetY =
+                        decelerateInterpolator.getInterpolation(dy / 2 / mPullHeight) * dy / 2
+                    mChildView!!.translationY = offsetY
+                    mHeader!!.layoutParams.height = offsetY.toInt()
+                    mHeader!!.requestLayout()
                 }
-
-
-                return true;
-
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
+                true
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                 if (mChildView != null) {
-                    if (mChildView.getTranslationY() >= mHeaderHeight) {
-                        mUpBackAnimator.start();
-                        mHeader.releaseDrag();
-                        mIsRefreshing = true;
-                        if (onCircleRefreshListener!=null) {
-                            onCircleRefreshListener.refreshing();
+                    if (mChildView!!.translationY >= mHeaderHeight) {
+                        mUpBackAnimator!!.start()
+                        mHeader!!.releaseDrag()
+                        mIsRefreshing = true
+                        if (onCircleRefreshListener != null) {
+                            onCircleRefreshListener!!.refreshing()
                         }
-
                     } else {
-                        float height = mChildView.getTranslationY();
-                        ValueAnimator backTopAni = ValueAnimator.ofFloat(height, 0);
-                        backTopAni.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                            @Override
-                            public void onAnimationUpdate(ValueAnimator animation) {
-                                float val = (float) animation.getAnimatedValue();
-                                val = decelerateInterpolator.getInterpolation(val / mHeaderHeight) * val;
-                                if (mChildView != null) {
-                                    mChildView.setTranslationY(val);
-                                }
-                                mHeader.getLayoutParams().height = (int) val;
-                                mHeader.requestLayout();
+                        val height = mChildView!!.translationY
+                        val backTopAni = ValueAnimator.ofFloat(height, 0f)
+                        backTopAni.addUpdateListener { animation ->
+                            var `val` = animation.animatedValue as Float
+                            `val` =
+                                decelerateInterpolator.getInterpolation(`val` / mHeaderHeight) * `val`
+                            if (mChildView != null) {
+                                mChildView!!.translationY = `val`
                             }
-                        });
-                        backTopAni.setDuration((long) (height * BACK_TOP_DUR / mHeaderHeight));
-                        backTopAni.start();
+                            mHeader!!.layoutParams.height = `val`.toInt()
+                            mHeader!!.requestLayout()
+                        }
+                        backTopAni.duration = (height * BACK_TOP_DUR / mHeaderHeight).toLong()
+                        backTopAni.start()
                     }
                 }
-                return true;
-            default:
-                return super.onTouchEvent(event);
+                true
+            }
+            else -> super.onTouchEvent(event)
         }
     }
 
-    public void finishRefreshing() {
+    fun finishRefreshing() {
         if (onCircleRefreshListener != null) {
-            onCircleRefreshListener.completeRefresh();
+            onCircleRefreshListener!!.completeRefresh()
         }
-        mIsRefreshing = false;
-        mHeader.setRefreshing(false);
+        mIsRefreshing = false
+        mHeader!!.setRefreshing(false)
     }
 
-    private OnCircleRefreshListener onCircleRefreshListener;
-
-    public void setOnRefreshListener(OnCircleRefreshListener onCircleRefreshListener) {
-        this.onCircleRefreshListener = onCircleRefreshListener;
+    private var onCircleRefreshListener: OnCircleRefreshListener? = null
+    fun setOnRefreshListener(onCircleRefreshListener: OnCircleRefreshListener?) {
+        this.onCircleRefreshListener = onCircleRefreshListener
     }
 
-    public interface OnCircleRefreshListener {
-        void completeRefresh();
+    interface OnCircleRefreshListener {
+        fun completeRefresh()
+        fun refreshing()
+    }
 
-        void refreshing();
+    companion object {
+        private const val TAG = "pullToRefresh"
+        private const val BACK_TOP_DUR: Long = 600
+        private const val REL_DRAG_DUR: Long = 200
+    }
+
+    init {
+        init(context, attrs, defStyleAttr)
     }
 }
